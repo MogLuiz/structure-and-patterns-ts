2ad3b566e0ab93e804e31de5606b2e9105a8a6be
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-09 23:39:11 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+import './database/index'
+
 const app = express();
 
+app.use(express.json())
 app.use(routes);
 
 app.listen(3333, () => {
